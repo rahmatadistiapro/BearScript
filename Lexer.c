a472708bfdbd14cf03235948755943ee61018d24
RAHMAT <rahmat.adistia@sat.co.id>
2025-12-18 23:20:51 +0700

    made the Lexer.c im just starting to make the skeleton next i'll try to make it emit tokens

---
+++ Lexer.c
@@ -0,0 +1,88 @@
+#include <ctype.h>
+#include "Token.h"
+#include "Lexer.h"
+
+static void lex_number(Lexer* lexer, Token* token);
+
+static void lex_number(Lexer* lexer, Token* token) {
+    int start_pos = lexer->position;
+    int has_dot = 0;
+    while (isdigit(lexer->current_char) || lexer->current_char == '.') {
+        if (lexer->current_char == '.') {
+            if (has_dot) {
+                break;
+            }
+            has_dot = 1;
+        }
+        advance(lexer);
+    }
+    int length = lexer->position - start_pos;
+
+    token->type = has_dot ? T_FLOAT : T_INTEGER;
+    token->value = (char*)malloc(length + 1);
+
+    memcpy(token->value, lexer->source + start_pos, length);
+    token->value[length] = '\0';
+}
+
+void lexer_init(Lexer* lexer, const char* source) {
+    lexer->source = source;
+    lexer->position = 0;
+    lexer->current_char = source[0];
+    lexer->line = 1;
+    lexer->column = 1;
+};
+
+void advance(Lexer* lexer) {
+    if (lexer->current_char == '\n') {
+        lexer->line++;
+        lexer->column = 0;
+    } else{
+        lexer->column++;
+    }
+    lexer->position++;
+    lexer->current_char = lexer->source[lexer->position];
+};
+
+void skip_whitespace(Lexer* lexer) {
+    while (isspace(lexer->current_char)) {
+        advance(lexer);
+    }
+};
+
+void make_token(Lexer* lexer, Token* token) {
+    skip_whitespace(lexer);
+    char current_char = lexer->current_char;
+
+    // clean previous value
+    if (token->value) {
+        free(token->value);
+        token->value = NULL;
+    }
+
+    if (current_char == '\0') {
+        token->type = T_EOF;
+        token->value = NULL;
+        return;
+    }
+
+    if (isdigit(current_char)) {
+        lex_number(lexer, token);
+        return;
+    }
+    else if (current_char == '+') {
+        token->type = T_ADD;
+        token->value = malloc(2);
+        token->value[0] = current_char;
+        token->value[1] = '\0';  // REQUIRED
+        advance(lexer);
+        return;
+    }
+    else {
+        token->type = T_UNKNOWN;
+        token->value = malloc(2);
+        token->value[0] = current_char;
+        token->value[1] = '\0';  // REQUIRED
+        advance(lexer);
+    }
+};
